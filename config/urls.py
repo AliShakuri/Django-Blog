6a089d351de1578c0cd2bda0faf2aa630fc3077d
alishakuri <ali95shakur@gmail.com>
2021-06-17 16:38:31 +0430

    v-069 send email for register

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from account.views import Login
+from account.views import Login, Register, activate
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('', include('django.contrib.auth.urls')),
     path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ]
