6d214f8c084cac3afe5516c6a2eae65c1a1c586d
alishakuri <ali95shakur@gmail.com>
2021-06-19 19:33:47 +0430

    v-74 add star rating

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_gravatar',
     'comment',
+    'star_ratings'
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,8 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+STAR_RATINGS_STAR_HEIGHT = 18
+# STAR_RATINGS_ANONYMOUS = True
+# STAR_RATINGS_RERATE = False
+# STAR_RATINGS_STAR_SPRITE = BASE_DIR / 'img.jpg'
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from account.views import Login, Register, activate
 
 urlpatterns = [
@@ -24,9 +24,10 @@ urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('register/', Register.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', activate, name='activate'),
+    re_path(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
-     path('comment/', include('comment.urls')),
+    path('comment/', include('comment.urls')),
 ]
 
 from django.conf.urls.static import static
